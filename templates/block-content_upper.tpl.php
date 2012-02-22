76a6896396ff76583ea6a05a47735f0fbd487a95
Brian Young <brianyoung@stanford.edu>
2012-02-21 16:23:18 -0800

    Moved all page templates to templates directory

---
+++ templates/block-content_upper.tpl.php
@@ -0,0 +1,34 @@
+<?php
+$edit_links = l(t('edit block'), 'admin/build/block/configure/'. $block->module .'/'. $block->delta, array('title' => t('edit the content of this block'), 'class' => 'block-edit'), drupal_get_destination(), NULL, FALSE, TRUE);
+?>
+
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="<?php
+
+if (((region_has_block('left')) && (region_has_block('right'))) && (($block_count == 4) && ($block_id == 1))): print 'span2';
+elseif (((region_has_block('left')) && (region_has_block('right'))) && (($block_count == 4) && ($block_id == 2))): print 'span2';
+elseif (((region_has_block('left')) && (region_has_block('right'))) && ($block_count == 4)) : print 'span1'; 
+elseif (((region_has_block('left')) && (region_has_block('right'))) && ($block_count == 3)) : print 'span2'; 
+elseif (((region_has_block('left')) && (region_has_block('right'))) && ($block_count == 2)) : print 'span3';
+elseif (((region_has_block('left')) && (region_has_block('right'))) && ($block_count == 1)) : print 'span6'; 
+
+elseif (((region_has_block('left')) || (region_has_block('right'))) && (($block_count == 4) && ($block_id == 1))): print 'span3';
+elseif (((region_has_block('left')) || (region_has_block('right'))) && ($block_count == 4)) : print 'span2'; 
+elseif (((region_has_block('left')) || (region_has_block('right'))) && ($block_count == 3)) : print 'span3'; 
+elseif (((region_has_block('left')) || (region_has_block('right'))) && (($block_count == 2) && ($block_id == 1))): print 'span6';
+elseif (((region_has_block('left')) || (region_has_block('right'))) && (($block_count == 2) && ($block_id == 2))): print 'span3'; 
+elseif (((region_has_block('left')) || (region_has_block('right'))) && ($block_count == 1)) : print 'span9'; 
+
+elseif ($block_count == 4) : print 'span3';
+elseif ($block_count == 3) : print 'span4';
+elseif ($block_count == 2) : print 'span6';
+else: print 'span12'; 
+endif; 
+?> block block-<?php print $block->module ?>">
+  <?php if ($block->subject): ?>
+  <h2><?php print $block->subject ?></h2>
+  <?php endif;?>
+  <div class="content"><?php print $block->content ?></div>
+  <?php if (user_access('administer blocks')) :?>
+  <button class="btn"><i class="icon-edit"></i> <?php print $edit_links; ?></button>
+  <?php endif; ?>
+</div>
