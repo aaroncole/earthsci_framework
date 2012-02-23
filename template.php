d655a385614463bf50a2c1dbd6c5c61c61701b79
Brian Young <brianyoung@stanford.edu>
2012-02-22 17:48:38 -0800

    Added disabled class to the pagination ellipsis

--- template.php
+++ template.php
@@ -165,8 +165,8 @@ function stanford_bootstrap_pager($tags = array(), $limit = 10, $element = 0, $p
     if ($i != $pager_max) {
       if ($i > 1) {
         $items[] = array(
-          'class' => '',
-          'data' => '',
+          'class' => 'disabled',
+          'data' => '<a href="#">…</a>',
         );
       }
       // Now generate the actual pager piece.
@@ -192,8 +192,8 @@ function stanford_bootstrap_pager($tags = array(), $limit = 10, $element = 0, $p
       }
       if ($i < $pager_max) {
         $items[] = array(
-          'class' => '',
-          'data' => '',
+          'class' => 'disabled',
+          'data' => '<a href="#">…</a>',
         );
       }
     }
